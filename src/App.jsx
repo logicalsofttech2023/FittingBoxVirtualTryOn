1b02f2e650c2ba59b9e423418be05bdffa60ee54
logicalsofttech2023 <logicalsofttech2023@gmail.com>
2025-09-13 17:14:19 +0530

    code

--- src/App.jsx
+++ src/App.jsx
@@ -6,32 +6,36 @@ function App() {
   const [showPopup, setShowPopup] = useState(false);
 
   useEffect(() => {
-    const params = {
-      apiKey: "TBVAcXitApiZPVH791yxdHbAc8AKzBwtCnjtv6Xn",
-      frame: "8053672909258",
-      onStopVto: hide,
-      onIssue: (data) => {
-        console.log("Issue:", data);
-      },
-      onSnapshot: (snapshot) => {
-        console.log("Snapshot taken:", snapshot);
-      },
-    };
+    if (showPopup && fitmixRef.current) {
+      const params = {
+        apiKey: "TBVAcXitApiZPVH791yxdHbAc8AKzBwtCnjtv6Xn",
+        frame: "8053672909258",
+        onStopVto: hide,
+        onIssue: (data) => {
+          console.log("Issue:", data);
+        },
+        onSnapshot: (snapshot) => {
+          console.log("Snapshot taken:", snapshot);
+        },
+      };
 
-    window.fitmixInstance = window.FitMix.createWidget(
-      "fitmix-container",
-      params,
-      function () {
-        console.log("VTO module is ready.");
-      }
-    );
+      window.fitmixInstance = window.FitMix.createWidget(
+        "fitmix-container",
+        params,
+        function () {
+          console.log("VTO module is ready.");
+          window.fitmixInstance.startVto("live");
+        }
+      );
+    }
 
     return () => {
       if (window.fitmixInstance) {
         window.fitmixInstance.stopVto();
+        window.fitmixInstance = null;
       }
     };
-  }, []);
+  }, [showPopup]);
 
   const hide = () => {
     if (fitmixRef.current) {
@@ -39,57 +43,48 @@ function App() {
     }
   };
 
-  const show = () => {
-    if (fitmixRef.current) {
-      fitmixRef.current.style.display = "block";
-    }
-  };
-
-  const openVto = (mode = "live") => {
-    window.fitmixInstance.startVto(mode);
-    show();
+  const openVto = () => {
     setShowPopup(true);
   };
 
   const stopVto = () => {
-    window.fitmixInstance.stopVto();
-    hide();
+    if (window.fitmixInstance) {
+      window.fitmixInstance.stopVto();
+    }
     setShowPopup(false);
   };
 
   const switchCamera = () => {
-    window.fitmixInstance.switchCamera();
+    window.fitmixInstance?.switchCamera();
   };
 
   const takeSnapshot = () => {
-    window.fitmixInstance.takeSnapshot();
+    window.fitmixInstance?.takeSnapshot();
   };
 
   const getPD = async () => {
-    const pd = await window.fitmixInstance.getPD();
-    alert("Your PD: " + JSON.stringify(pd));
+    if (window.fitmixInstance) {
+      const pd = await window.fitmixInstance.getPD();
+      alert("Your PD: " + JSON.stringify(pd));
+    }
   };
 
   return (
     <div className="wrapper">
-      {/* Frame Selection */}
       <div>
-        <button onClick={() => window.fitmixInstance.setFrame("8053672909258")}>
+        <button onClick={() => window.fitmixInstance?.setFrame("8053672909258")}>
           Sample frame 1
         </button>
-        <button onClick={() => window.fitmixInstance.setFrame("0888392486523")}>
+        <button onClick={() => window.fitmixInstance?.setFrame("0888392486523")}>
           Sample frame 2
         </button>
-        <button onClick={() => window.fitmixInstance.setFrame("8056597233958")}>
+        <button onClick={() => window.fitmixInstance?.setFrame("8056597233958")}>
           Sample frame 3
         </button>
       </div>
 
-      {/* Open Popup */}
-      <button onClick={() => openVto("live")}>Start Live VTO</button>
-      <button onClick={() => openVto("photo")}>Start Photo VTO</button>
+      <button onClick={openVto}>Start VTO</button>
 
-      {/* Popup Modal */}
       {showPopup && (
         <div className="popup">
           <div className="popup-content">
